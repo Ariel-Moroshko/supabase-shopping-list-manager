0081b9108a6f5abc1e663e0f2f588b4c7095f7a9
Ariel-Moroshko <45500865+Ariel-Moroshko@users.noreply.github.com>
2024-01-17 16:45:30 +0200

    Update index.ts

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,6 +2,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "process.env.DATABASE_URL was not found! maybe you forgot to set it?",
+  );
+}
+
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 export const db = drizzle(client, { schema, logger: true });
