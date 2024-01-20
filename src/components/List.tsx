e0514ff5560fa149149f5cc7f92b45567147795e
Ariel-Moroshko <45500865+Ariel-Moroshko@users.noreply.github.com>
2024-01-20 17:44:44 +0200

    Update List.tsx

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,11 +5,6 @@ import AllItemsList from "./AllItemsList";
 import { useTabContext } from "@/hooks/useTabContext";
 import ShoppingList from "./ShoppingList";
 import { useEffect } from "react";
-import { getSupabaseBrowserClient } from "@/lib/supabase/browserClient";
-import {
-  REALTIME_CHANNEL_STATES,
-  RealtimePostgresChangesPayload,
-} from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
 import { useNetworkStatus } from "@/hooks/useNetworkStatus";
 import SignOut from "./SignOut";
@@ -22,83 +17,28 @@ export default function ListPage({ list }: Props) {
   const networkStatus = useNetworkStatus();
 
   useEffect(() => {
-    let supabase = getSupabaseBrowserClient();
-    const handleRealTimeEvent = (
-      payload: RealtimePostgresChangesPayload<{
-        [key: string]: any;
-      }>,
-    ) => {
-      console.log(payload);
+    let refreshIntervalId = window.setInterval(() => {
       router.refresh();
+    }, 5000);
+    const onVisibilityChange = () => {
+      if (document.visibilityState === "hidden") {
+        console.log("hidden, clearing interval");
+        window.clearInterval(refreshIntervalId);
+      } else {
+        console.log("visible, refreshing and setting interval");
+        router.refresh();
+        refreshIntervalId = window.setInterval(() => {
+          router.refresh();
+        }, 5000);
+      }
     };
-
-    const subscribeToUpdates = () => {
-      return supabase
-        .channel("list-changes")
-        .on(
-          "postgres_changes",
-          {
-            event: "*",
-            schema: "public",
-            table: "items",
-          },
-          handleRealTimeEvent,
-        )
-        .on(
-          "postgres_changes",
-          {
-            event: "*",
-            schema: "public",
-            table: "categories",
-          },
-          handleRealTimeEvent,
-        )
-        .subscribe(
-          async (
-            status: "SUBSCRIBED" | "TIMED_OUT" | "CLOSED" | "CHANNEL_ERROR",
-            error?: Error,
-          ) => {
-            console.log(
-              "*".repeat(20),
-              new Date().toLocaleString(),
-              "status/error changed. status is:",
-              status,
-              "error is:",
-              error?.name,
-              ":",
-              error?.message,
-            );
-            if (channel.state === REALTIME_CHANNEL_STATES.errored) {
-              console.log(
-                "----> REALTIME_CHANNEL_STATES.errored, removing channel and subscribing again",
-              );
-            }
-            if (status === "CLOSED" && !isReSubscribing) {
-              console.log(
-                "***** channel was CLOSED! removing it and resubscribing...",
-              );
-              isReSubscribing = true;
-              await supabase.removeChannel(channel);
-              const authResponse = await supabase.auth.refreshSession();
-              supabase = getSupabaseBrowserClient();
-              console.log("got authResponse=", authResponse);
-              channel = subscribeToUpdates();
-              isReSubscribing = false;
-            }
-          },
-        );
-    };
-
-    console.log("subscribing to channel updates");
-    let isReSubscribing = false;
-    let channel = subscribeToUpdates();
-
+    document.addEventListener("visibilitychange", onVisibilityChange);
     return () => {
-      console.log("removing ws");
-      isReSubscribing = true;
-      supabase.removeChannel(channel);
+      document.removeEventListener("visibilitychange", onVisibilityChange);
+      window.clearInterval(refreshIntervalId);
     };
-  }, [router]);
+  });
+
   return (
     <>
       <SignOut />
